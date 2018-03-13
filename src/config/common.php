8a872bea457e19826e1b74485481f8a16ac69f7a
Andrii Vasyliev <sol@hiqdev.com>
2018-03-13 15:07:44 +0200

    simplified config, moved to hiapi and yii2-data-mapper

--- src/config/common.php
+++ src/config/common.php
@@ -8,27 +8,16 @@
  * @copyright Copyright (c) 2018, TransMedia (http://transmedia.com.ua/)
  */
 
-/**
- * @var array $params
- */
-
-use yii\di\Instance;
-
 return [
     'container' => [
         'singletons' => [
-            \hiqdev\yii\DataMapper\hydrator\ConfigurableAggregateHydrator::class => [
-                'hydrators' => [
-                     //\transmedia\signage\api\models\File::class => \transmedia\signage\api\models\FileHydrator::class,
-                 ],
-            ],
-            // Command bus:
-            \League\Tactician\Handler\CommandNameExtractor\CommandNameExtractor::class => \League\Tactician\Handler\CommandNameExtractor\ClassNameExtractor::class,
-            \League\Tactician\Handler\Locator\HandlerLocator::class => \hiqdev\yii2\autobus\bus\NearbyHandlerLocator::class,
-            \League\Tactician\Handler\MethodNameInflector\MethodNameInflector::class => \League\Tactician\Handler\MethodNameInflector\HandleInflector::class,
-
-            \hiqdev\yii2\autobus\components\CommandFactoryInterface::class => \hiqdev\yii2\autobus\components\SimpleCommandFactory::class,
-
+        /// User
+            \yii\web\User::class => function () {
+                return new \yii\web\User([
+                    'identityClass' => \transmedia\hiapi\models\UserIdentity::class,
+                ]);
+            },
+        // BUS
             \hiapi\bus\ApiCommandsBusInterface::class => [
                 'branches' => [
                     'file' => [
@@ -38,23 +27,7 @@ return [
                 ],
             ],
 
-            'bus.http-request' => [
-                '__class' => \hiqdev\yii2\autobus\components\TacticianCommandBus::class,
-                '__construct()' => [
-                    Instance::of('bus.http-request.default-command-handler'),
-                ],
-                'middlewares' => [
-                    $_ENV['ENABLE_JSONAPI_RESPONSE'] ?? false
-                        ? \hiapi\middlewares\JsonApiMiddleware::class
-                        : \hiapi\middlewares\LegacyResponderMiddleware::class,
-                    \hiapi\middlewares\HandleExceptionsMiddleware::class,
-                    \hiqdev\yii2\autobus\bus\LoadFromRequestMiddleware::class,
-                    \hiqdev\yii2\autobus\bus\ValidateMiddleware::class,
-                    'bus.http-request.per-command-middleware',
-                ],
-            ],
-            'bus.http-request.per-command-middleware' => [
-                '__class' => \hiapi\middlewares\PerCommandMiddleware::class,
+            'bus.per-command-middleware' => [
                 'commandMiddlewares' => [
                     \transmedia\signage\file\api\commands\FileSearchCommand::class => [
                         [\hiapi\middlewares\AuthMiddleware::class, 'file.read'],
@@ -66,45 +39,6 @@ return [
                     ],
                 ],
             ],
-            'bus.http-request.default-command-handler' => [
-                '__class' => \League\Tactician\Handler\CommandHandlerMiddleware::class,
-                '__construct()' => [
-                    Instance::of(\League\Tactician\Handler\CommandNameExtractor\ClassNameExtractor::class),
-                    Instance::of(\hiqdev\yii2\autobus\bus\NearbyHandlerLocator::class),
-                    Instance::of(\League\Tactician\Handler\MethodNameInflector\HandleInflector::class),
-                ],
-            ],
-
-            // Data-access layer:
-            \Zend\Hydrator\HydratorInterface::class => function ($container) {
-                return $container->get(\hiqdev\yii\DataMapper\hydrator\ConfigurableAggregateHydrator::class);
-            },
-
-            // Domain layer related:
-            \hiqdev\yii\DataMapper\components\EntityManagerInterface::class => [
-                '__class' => \hiqdev\yii\DataMapper\components\EntityManager::class,
-                'repositories' => [
-                ],
-            ],
-
-            // General
-            \yii\di\Container::class => function ($container) {
-                return $container;
-            },
-            \Psr\Http\Message\ServerRequestInterface::class => function ($container) {
-                return \GuzzleHttp\Psr7\ServerRequest::fromGlobals();
-            },
-            \Psr\Http\Message\ResponseInterface::class => function ($container) {
-                return new \GuzzleHttp\Psr7\Response();
-            },
-            \WoohooLabs\Yin\JsonApi\Request\RequestInterface::class => \WoohooLabs\Yin\JsonApi\Request\Request::class,
-            \WoohooLabs\Yin\JsonApi\Exception\ExceptionFactoryInterface::class => \WoohooLabs\Yin\JsonApi\Exception\DefaultExceptionFactory::class,
-
-            \yii\web\User::class => function () {
-                return new \yii\web\User([
-                    'identityClass' => \transmedia\hiapi\models\UserIdentity::class,
-                ]);
-            },
-        ]
-    ]
+        ],
+    ],
 ];
