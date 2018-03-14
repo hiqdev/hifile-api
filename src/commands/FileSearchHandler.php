562dd9b4a8f1793bbb755f792cc7a79cf91b5886
Andrii Vasyliev <sol@hiqdev.com>
2018-03-14 11:22:38 +0200

    added `FileSearchHandler`

---
+++ src/commands/FileSearchHandler.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace transmedia\signage\file\api\commands;
+
+use hiapi\commands\SearchHandler;
+
+/**
+ * Class FileSearchCommand
+ *
+ * @author Andrii Vasyliev <sol@hiqdev.com>
+ */
+class FileSearchHandler extends SearchHandler
+{
+}
