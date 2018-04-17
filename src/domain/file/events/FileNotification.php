56087e901df661f05ec0f9a309e62036c483eef7
Andrii Vasyliev <sol@hiqdev.com>
2018-04-17 19:27:23 +0300

    fixed file notification to serialize file object with hydrator

--- src/domain/file/events/FileNotification.php
+++ src/domain/file/events/FileNotification.php
@@ -2,11 +2,31 @@
 
 namespace transmedia\signage\file\api\domain\file\events;
 
+use transmedia\signage\file\api\domain\AbstractEvent;
+
 /**
  * Class FileNotification
  *
  * @author Andrii Vasyliev <sol@hiqdev.com>
  */
-final class FileNotification extends FileEvent
+final class FileNotification extends AbstractEvent
 {
+    /**
+     * @var array
+     */
+    protected $target;
+
+    /**
+     * AbstractEvent constructor.
+     * @param object $target
+     */
+    public function __construct(array $target)
+    {
+        parent::__construct($target);
+    }
+
+    public function jsonSerialize()
+    {
+        return array_merge(parent::jsonSerialize(), $this->target);
+    }
 }
--- src/services/NotifyToQueue.php
+++ src/services/NotifyToQueue.php
@@ -2,6 +2,8 @@
 
 namespace transmedia\signage\file\api\services;
 
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use Psr\Log\LoggerInterface;
 use hiapi\event\PublishToQueueListener;
 use transmedia\signage\file\api\domain\file\File;
 use transmedia\signage\file\api\domain\file\events\FileNotification;
@@ -13,11 +15,17 @@ use transmedia\signage\file\api\domain\file\events\FileNotification;
  */
 class NotifyToQueue extends PublishToQueueListener implements FileNotifierInterface
 {
+    public function __construct(AMQPStreamConnection $amqp, LoggerInterface $logger, FileHydrator $hydrator)
+    {
+        parent::__construct($amqp, $logger);
+        $this->hydrator = $hydrator;
+    }
+
     /**
      * @param File $file
      */
     public function notify(File $file): void
     {
-        $this->handle(new FileNotification($file));
+        $this->handle(new FileNotification($this->hydrator->extract($file)));
     }
 }
